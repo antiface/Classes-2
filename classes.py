2c66d988e035a0e31bbb83ef30a4d6cd440fdf45
David McMahon <davidmcmhn@gmail.com>
2015-04-05 18:39:36 +0100

    Initial Commit

---
+++ classes.py
@@ -0,0 +1,19 @@
+class Musician(object):
+    def __init__(self, sounds):
+        self.sounds = sounds
+
+    def solo(self, length):
+        for i in range(length):
+            print(self.solo([i % len(self.sounds)]))
+
+    # def saySomething(self):
+    #     print "Say Something method within Musician Class"
+
+class Bassist(Musician):
+    def __init__(self):
+        super(Bassist, self).__init__(["Twang", "Slap", "Boom"])
+
+
+if __name__=="__main__":
+    dave = Bassist()
+    dave.solo()
