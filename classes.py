952f3fef51c23f7c10e4a5d5a9022fd03e8c649a
David McMahon <davidmcmhn@gmail.com>
2015-04-05 20:01:04 +0100

    Guitar class added

--- classes.py
+++ classes.py
@@ -13,6 +13,14 @@ class Bassist(Musician):
     def __init__(self):
         super(Bassist, self).__init__(["Twang", "Slap", "Boom"])
 
+class Guitarist(Musician):
+    def __init__(self):
+        super(Guitarist, self).__init__(["Pluck", "Strum", "Chuck"])
+
+    def tune(self):
+        print "I'll be with you in a minute"
+        print "Twing, Ping"
+
 
 if __name__=="__main__":
     dave = Bassist()
